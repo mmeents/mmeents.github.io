dad73cb85a01ac83d403dfaa91129f9aaa726e91
mmeents <mmeents@users.noreply.github.com>
2019-07-10 14:08:22 -0400

    more menu edits

--- MainMenu.js
+++ MainMenu.js
@@ -1,9 +1,10 @@
-﻿function PrintMainMenu(sTitle) {
+﻿function PrintMainMenu(sPageName, sTitle) {
    // <nav class="navbar navbar-fixed-top navbar-logo navbar " > </nav>
   let sOut = '<div class="container-fluid">' +
     '<div class="navbar-header">' +
-      '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>' +
-      '<h3>'+sTitle+'</h3>' +
+    '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>' +
+    '<h3><a href="' + (sPageName == "default.html" ? "https://github.com/mmeents" : "default.html") + '" style="font-size:large;color:black;font-weight:bolder;font-family:Arial, Helvetica, sans-serif;">' +
+      sTitle + '</a></h3>' +
     '</div>' +
     '<div id="navbar" class="navbar-collapse collapse">' +
       '<ul class="nav navbar-nav navbar-right">' +
