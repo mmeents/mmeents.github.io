d0eb31d966b574189c4c25b4f7dd70f8b559795a
mmeents <mmeents@users.noreply.github.com>
2019-08-01 22:04:35 -0400

    tak3

--- MainMenu.js
+++ MainMenu.js
@@ -12,7 +12,7 @@
         '<li class="dropdown">' +
           '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> Links <span class="caret"></span></a>' +
           '<ul class="dropdown-menu" role="menu">' +
-            '<li><a href="gravity/gravity.html">Digital Solar System</a></li>' +
+            '<li><a href="gravity//gravity.html">Digital Solar System</a></li>' +
             '<li class="divider"></li>' +
             '<li><a href="CryptoWatch.html">Big List of Crypto</a></li>' +
             '<li><a href="BigCryptoChart.html">Big Charts</a></li>' +
