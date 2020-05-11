42e4d8f704d797687dc4dee9ca0c5dabda66d30d
Matt Meents <mmeents@users.noreply.github.com>
2020-05-11 14:33:29 -0400

    remove h3

--- MainMenu.js
+++ MainMenu.js
@@ -3,8 +3,8 @@
   let sOut = '<div class="container-fluid">' +
     '<div class="navbar-header">' +
     '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>' +
-    '<h3><a href="' + (sPageName == "default.html" ? "https://github.com/mmeents" : "default.html") + '" style="font-size:large;color:black;font-weight:bolder;font-family:Arial, Helvetica, sans-serif;">' +
-      sTitle + '</a></h3>' +
+    '<a href="' + (sPageName == "default.html" ? "https://github.com/mmeents" : "default.html") + '" style="font-size:large;color:black;font-weight:bolder;font-family:Arial, Helvetica, sans-serif;">' +
+      sTitle + '</a>' +
     '</div>' +
     '<div id="navbar" class="navbar-collapse collapse">' +
       '<ul class="nav navbar-nav navbar-right">' +
